dd0b95296063d2cf799eff44f5b2383236409c16
Alexander Schlindwein <alexander.schlindwein@hotmail.de>
2020-03-12 11:37:51 +0100

    Update factory

--- contracts/interfaces/IUniswapFactory.sol
+++ contracts/interfaces/IUniswapFactory.sol
@@ -7,5 +7,5 @@ interface IUniswapFactory {
   function createExchange(address token) external returns (address payable);
   function getExchange(address token) external view returns (address payable);
   function getToken(address token) external view returns (address);
-  function getTokenWihId(uint256 token_id) external view returns (address);
+  function getTokenWithId(uint256 token_id) external view returns (address);
 }
--- contracts/uniswap/UniswapFactory.sol
+++ contracts/uniswap/UniswapFactory.sol
@@ -1,6 +1,7 @@
 pragma solidity ^0.5.6;
 
 import "./UniswapExchange.sol";
+import "../interfaces/IUniswapFactory.sol";
 import "../interfaces/IUniswapExchange.sol";
 
 
@@ -14,6 +15,12 @@ contract UniswapFactory {
 
   address public exchangeTemplate;
   uint256 public tokenCount;
+
+  address public adx;
+  UniswapExchange public adxExchange;
+  uint public adxExchangeId;
+  IUniswapFactory public realUniswapFactory;
+
   mapping (address => address) internal token_to_exchange;
   mapping (address => address) internal exchange_to_token;
   mapping (uint256 => address) internal id_to_token;
@@ -22,41 +29,53 @@ contract UniswapFactory {
   |         Factory Functions         |
   |__________________________________*/
 
-  function initializeFactory(address template) public {
-    assert(exchangeTemplate == address(0));
-    assert(template != address(0));
-    exchangeTemplate = template;
-  }
-  
-  function createExchange(address token) public returns (address) {
-    assert(token != address(0));
-    assert(exchangeTemplate != address(0));
-    assert(token_to_exchange[token] == address(0));
+  function initializeFactory(address _template, address _adx, address _realUniswapFactory) public {
+    require(exchangeTemplate == address(0), "exchange template must be zero address");
+    require(_template != address(0), "template must not be zero address");
+    require(_adx != address(0), "adx must not be zero address");
+    require(_realUniswapFactory != address(0), "real uniswap factory must not be zero address");
+
+    exchangeTemplate = _template;
+
+    adx = _adx;
+    realUniswapFactory = IUniswapFactory(_realUniswapFactory);
+
     UniswapExchange exchange = new UniswapExchange();
-    exchange.setup(token);
-    token_to_exchange[token] = address(exchange);
-    exchange_to_token[address(exchange)] = token;
-    uint256 token_id = tokenCount + 1;
-    tokenCount = token_id;
-    id_to_token[token_id] = token;
-    emit NewExchange(token, address(exchange));
-    return address(exchange);
+    exchange.setup(adx);
+
+    adxExchange = exchange;
+    adxExchangeId = 1;
+    tokenCount = 1;
+
+    emit NewExchange(adx, address(exchange));
   }
 
   /***********************************|
   |         Getter Functions          |
   |__________________________________*/
 
+
+  // All getters redirect to real Uniswap for all tokens exept ADX.
+
   function getExchange(address token) public view returns (address) {
-    return token_to_exchange[token];
+    if (token == adx) {
+      return address(adxExchange);
+    }
+    return realUniswapFactory.getExchange(token);
   }
 
   function getToken(address exchange) public view returns (address) {
-    return exchange_to_token[exchange];
+    if (exchange == address(adxExchange)) {
+      return adx;
+    }
+    return realUniswapFactory.getToken(exchange);
   }
 
-  function getTokenWithId(uint256 token_id) public view returns (address) {
-    return id_to_token[token_id];
+  function getTokenWithId(uint256 tokenId) public view returns (address) {
+    if (tokenId == adxExchangeId) {
+      return adx;
+    }
+    return realUniswapFactory.getTokenWihId(tokenId);
   }
 
 }
