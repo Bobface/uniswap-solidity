9b15cefe7ca59b86fa36cce4fd4e219bf2e65de3
Alexander Schlindwein <alexander.schlindwein@hotmail.de>
2020-03-12 13:10:18 +0100

    Add mainnet addresses

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -4,8 +4,8 @@ module.exports = async function(deployer, network, accounts) {
 
     console.log(network);
 
-    let adxMainnet = "";
-    let uniswapMainnet = "";
+    let adxMainnet = "0x4470BB87d77b963A013DB939BE332f927f2b992e";
+    let uniswapMainnet = "0xc0a47dFe034B400B47bDaD5FecDa2621de6c4d95";
     
     await deployer.deploy(UniswapFactory, adxMainnet, uniswapMainnet);
 };
